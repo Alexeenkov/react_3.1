6ae8557e6441f9ce0f01073e51db63674a7f3280
Илья Юрьевич Алексеенков <74237440+Alexeenkov@users.noreply.github.com>
2022-08-24 21:46:58 +0300

    Add files via upload

---
+++ homework_3.1/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import Stars from './components/Stars/Stars';
+
+function App() {
+  return (
+    <Stars rating={5} />
+  );
+}
+
+export default App;
